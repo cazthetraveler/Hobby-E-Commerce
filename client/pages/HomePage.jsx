3f858d5d83d020e249e24fbe30769b6279405f09
drewhouses <drewcasas@live.com>
2024-02-05 19:03:50 -0600

    started on server.js, fixed main.jsx

---
+++ client/pages/HomePage.jsx
@@ -0,0 +1,6 @@
+const HomePage = () => {
+  //TODO 
+  return <h2>This is the HomePage</h2>;
+};
+
+export default HomePage;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App.jsx'
-import Home from './pages/Home';
+import App from "./App.jsx";
+import HomePage from "../pages/HomePage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -11,13 +11,12 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />
+        element: <HomePage />,
       },
-    ]
+    ],
   },
 ]);
 
-      
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
