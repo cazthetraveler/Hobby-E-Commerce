4a6f798eacd30e3118ab7da4fdd96a19ab863f6c
Evan Rice <evancr2017@gmail.com>
2024-02-08 20:40:29 -0600

    added to catagory

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,6 +1,47 @@
+import ItemCard from "../components/ItemCard";
+import { useLocation } from "react-router-dom";
+import { GET_ITEMS } from "../utils/queries";
+import { useQuery } from "@apollo/client";
+
 const Category = () => {
+    //gets catagory name
+    const currentPage = useLocation().pathname;
+    const title = currentPage.split("/").pop().trim();
+
+    const { loading, error, data } = useQuery(GET_ITEMS);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+    if (error) {
+    return <p>Error: {error.message}</p>;
+  } 
+
+  if (!data || !data.items) {
+    return <p>No featured items available.</p>;
+  }
+ console.log(data.items);
+  const callItem = data?.item || [];
+
+  const filteredItems = data.items.filter((item) =>
+    item.category.includes(item.category[0])
+  );
+
   //TODO
-  return <h2>This is the Categories page.</h2>;
+  return ( 
+  <div>
+      <h2 className="text-3xl font-bold">{title} Categories</h2>
+      <div>
+        <ul>
+          { callItem && callItem.map( (item) =>
+            <ItemCard key={item._id} item={item} />
+          )}
+        </ul>
+      </div>
+  </div>
+  
+  )
 };
 
 export default Category;
