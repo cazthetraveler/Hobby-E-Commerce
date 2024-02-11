958ba744de339eb44a609f4458b0ad15cfe3c153
MultiMouse <olmos.luisd@outlook.com>
2024-02-11 00:16:16 -0600

    if user is not logged in, dont show cart, open login instead

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,8 @@ const Header = ({ onToggleModal, onToggleNav, onToggleCart }) => {
           </>
         )}
 
-        <button onClick={onToggleCart}>
+        <button onClick={Auth.loggedIn() ? onToggleCart : onToggleModal}>
+          {/* if user is logged in, show cart, if not then open login modal */}
           <i className="fa-solid fa-cart-shopping text-3xl text-white hover:text-cyan-800 duration-200"></i>
         </button>
       </div>
