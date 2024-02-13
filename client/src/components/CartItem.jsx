58ef98e383c20f13a333529d80213c2bce251037
MultiMouse <olmos.luisd@outlook.com>
2024-02-12 21:15:43 -0600

    finally fixed the cart item display

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,14 +1,19 @@
 import { useMutation } from "@apollo/client";
 import { REMOVE_FROM_CART } from "../utils/mutations";
+import { GET_ONE_USER_CART } from "../utils/queries";
 
 const CartItem = ({ item, userId }) => {
   const { _id, itemName, price, image } = item;
 
-  const [removeFromCart, { error }] = useMutation(REMOVE_FROM_CART);
+  const [removeFromCart, { error }] = useMutation(REMOVE_FROM_CART, {
+    refetchQueries: [GET_ONE_USER_CART, "getUserCart"],
+  });
 
   const handleRemoveFromCart = async () => {
     try {
-      const { data } = await removeFromCart({ variables: { id: userId, itemId: _id } });
+      const { data } = await removeFromCart({
+        variables: { id: userId, itemId: _id },
+      });
     } catch (err) {
       console.log(err);
     }
